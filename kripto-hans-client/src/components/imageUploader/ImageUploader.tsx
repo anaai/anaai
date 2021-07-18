54648b4964f296451223226921881745233bf37b
Filip Kis <filip.kis.dev@gmail.com>
2021-07-19 00:42:26 +0200

    Add basic selected image preview

--- kripto-hans-client/src/components/imageUploader/ImageUploader.tsx
+++ kripto-hans-client/src/components/imageUploader/ImageUploader.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { ChangeEventHandler, useState } from "react";
 
-export const ImageUploader: React.FC<{}> = () => (
-  <>
-    <label htmlFor="image-upload-input">Upload image</label>
-    <input type="file" accept=".jpg,.jpeg,.png,.gif" id="image-upload-input" />
-  </>
-);
+export const ImageUploader: React.FC<{}> = () => {
+  const [clientImage, setClientImage] = useState<{
+    imageUrl: string | null;
+    image: File | null;
+  }>({
+    imageUrl: null,
+    image: null,
+  });
+
+  const handleImageSelect: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setClientImage({
+        imageUrl: URL.createObjectURL(event.target.files[0]),
+        image: event.target.files[0],
+      });
+    }
+  };
+  return (
+    <>
+      <label htmlFor="image-upload-input">Upload image</label>
+      <input
+        type="file"
+        accept=".jpg,.jpeg,.png,.gif"
+        id="image-upload-input"
+        onChange={handleImageSelect}
+      />
+
+      {clientImage && <img src={clientImage.imageUrl as string} alt="Selected"/>}
+    </>
+  );
+};
