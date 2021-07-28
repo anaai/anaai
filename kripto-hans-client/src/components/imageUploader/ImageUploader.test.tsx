f65bf885152877d2a55dff13c2c11c46e70a0b19
Filip Kis <filip.kis.dev@gmail.com>
2021-07-28 23:21:12 +0200

    Add image selection test

--- kripto-hans-client/src/components/imageUploader/ImageUploader.test.tsx
+++ kripto-hans-client/src/components/imageUploader/ImageUploader.test.tsx
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ImageUploader } from "./ImageUploader";
 
 test("renders image upload input", () => {
   render(<ImageUploader />);
   const inputElement = screen.getByLabelText("Upload image");
   expect(inputElement).toBeInTheDocument();
+});
+
+test("allows user to select an image", () => {
+  window.URL.createObjectURL = function () {} as any;
+  render(<ImageUploader />);
+  const inputElement = screen.getByLabelText(
+    "Upload image"
+  ) as HTMLInputElement;
+  const file = new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" });
+  fireEvent.change(inputElement, {
+    target: {
+      files: [file],
+    },
+  });
+  expect(inputElement.files![0]).toBe(file);
 });
