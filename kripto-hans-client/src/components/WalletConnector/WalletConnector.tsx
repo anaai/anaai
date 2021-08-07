e2945e7775503624df438ae53bfe22a3c834d075
Filip Kis <filip.kis.dev@gmail.com>
2021-08-07 22:52:02 +0200

    Add basic isMetaMaskInstallled check

--- kripto-hans-client/src/components/WalletConnector/WalletConnector.tsx
+++ kripto-hans-client/src/components/WalletConnector/WalletConnector.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 export const WalletConnector: React.FC<{}> = () => {
-    return <div>Wallet connector works!</div>
-}
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  useEffect(() => {
+    console.log(isMetaMaskInstalled());
+  }, []);
+
+  return <div>Wallet connector works!</div>;
+};
