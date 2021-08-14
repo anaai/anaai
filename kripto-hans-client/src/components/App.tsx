270dd6bb00d8d5e84ed3fa6005c7089453e7b4e3
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 21:15:35 +0200

    Add CssBaseline

--- kripto-hans-client/src/components/App.tsx
+++ kripto-hans-client/src/components/App.tsx
@@ -1,10 +1,16 @@
+import { CssBaseline } from '@material-ui/core';
 import 'components/App.scss';
 
 import { ReactElement } from 'react';
 import { LandingScene } from './LandingScene/LandingScene';
 
 function App(): ReactElement {
-  return <LandingScene />;
+  return (
+    <>
+      <CssBaseline />
+      <LandingScene />
+    </>
+  );
 }
 
 export default App;
