2033cd0f86f4ee8842b00d94eae6c20a422c7f9e
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 21:28:23 +0200

    Add primaryTheme typography config

--- kripto-hans-client/src/config/theme/primaryTheme.ts
+++ kripto-hans-client/src/config/theme/primaryTheme.ts
@@ -3,6 +3,10 @@ import { createTheme, responsiveFontSizes } from '@material-ui/core';
 // Theme palette: https://coolors.co/03256c-2541b2-1768ac-06bee1-ffffff
 export const primaryTheme = responsiveFontSizes(
   createTheme({
+    typography: {
+      fontFamily: 'Harmattan',
+      fontSize: 32
+    },
     palette: {
       primary: {
         main: '#1768ac'
