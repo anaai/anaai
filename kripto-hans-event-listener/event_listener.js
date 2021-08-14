606b863f38a6bf78bf29cdafb9eba7115b5de2ee
vonum <vonum.mk@gmail.com>
2021-08-14 18:37:28 +0200

    Use urls from .env

--- kripto-hans-event-listener/event_listener.js
+++ kripto-hans-event-listener/event_listener.js
@@ -3,9 +3,8 @@ const axios = require("axios")
 const uuidv4 = require("uuid/v4")
 
 const WS_API_URL = process.env.WS_API_URL;
-const PUBLIC_KEY = process.env.PUBLIC_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CARTOONIFY_URL = process.env.JOB_SERVICE_CARTOONIFY_URL;
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(WS_API_URL);
@@ -15,7 +14,7 @@ const nftContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS);
 
 const triggerJob = async (imageURL, imageName) => {
   const response = await axios.post(
-    "http://localhost:8000/cartoonify",
+    CARTOONIFY_URL,
     {image_url: imageURL, image_name: imageName}
   );
   const data = await response.data;
