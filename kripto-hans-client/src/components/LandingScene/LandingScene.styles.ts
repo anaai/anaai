cfb92b106cf5c2856a63801b25d0fc12e020735c
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 23:42:17 +0200

    Add initial LandingScene buttons

--- kripto-hans-client/src/components/LandingScene/LandingScene.styles.ts
+++ kripto-hans-client/src/components/LandingScene/LandingScene.styles.ts
@@ -4,9 +4,30 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     height: '100vh',
-    background: `linear-gradient(200deg, ${theme.palette.primary.dark} 0%, ${theme.palette.primary.main} 35%, ${theme.palette.secondary.light} 100%)`
+    background: `linear-gradient(200deg, ${theme.palette.primary.dark} 0%, ${theme.palette.primary.main} 35%, ${theme.palette.secondary.light} 100%)`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
   },
   heroTitle: {
-    color: '#fff'
+    color: '#fff',
+    userSelect: 'none'
+  },
+  ctaButtonsContainer: {},
+  generateButton: {
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+    marginRight: theme.spacing(1),
+    borderRadius: theme.spacing(2)
+  },
+  acquireButton: {
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+    marginLeft: theme.spacing(1),
+    borderRadius: theme.spacing(2),
+    color: '#fff',
+    backgroundColor: '#26b76e',
+    '&:hover': {
+      backgroundColor: '#14663D'
+    }
   }
 }));
--- kripto-hans-client/src/components/LandingScene/LandingScene.tsx
+++ kripto-hans-client/src/components/LandingScene/LandingScene.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
 import React from 'react';
 import { useStyles } from './LandingScene.styles';
 
@@ -7,8 +7,17 @@ export const LandingScene: React.FC<Record<string, unknown>> = () => {
   return (
     <Box className={classes.root}>
       <Typography variant="h1" className={classes.heroTitle}>
-        AI NFT ART
+        Ana
       </Typography>
+
+      <Box className={classes.ctaButtonsContainer}>
+        <Button className={classes.generateButton} color="primary" variant="contained">
+          Generate
+        </Button>
+        <Button className={classes.acquireButton} color="secondary" variant="contained">
+          Buy
+        </Button>
+      </Box>
     </Box>
   );
 };
