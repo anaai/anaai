6361c94414a82e9adb6ac3884caae28ce997294e
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 21:24:37 +0200

    Integrate primary theme

--- kripto-hans-client/src/components/App.tsx
+++ kripto-hans-client/src/components/App.tsx
@@ -1,15 +1,16 @@
-import { CssBaseline } from '@material-ui/core';
-import 'components/App.scss';
-
 import { ReactElement } from 'react';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+
+import 'components/App.scss';
+import { primaryTheme } from 'config/theme/primaryTheme';
 import { LandingScene } from './LandingScene/LandingScene';
 
 function App(): ReactElement {
   return (
-    <>
+    <ThemeProvider theme={primaryTheme}>
       <CssBaseline />
       <LandingScene />
-    </>
+    </ThemeProvider>
   );
 }
 
