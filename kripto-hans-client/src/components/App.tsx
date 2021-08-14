c8e6c4d93c7d54c4b1d665bef69cbda8ccb05f6b
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 21:13:56 +0200

    Refactor global styles

--- kripto-hans-client/src/components/App.tsx
+++ kripto-hans-client/src/components/App.tsx
@@ -1,16 +1,10 @@
-import 'shared/styles/Reset.scss';
-import 'shared/styles/Global.scss';
 import 'components/App.scss';
 
 import { ReactElement } from 'react';
 import { LandingScene } from './LandingScene/LandingScene';
 
 function App(): ReactElement {
-  return (
-    <section className="app-container">
-      <LandingScene />
-    </section>
-  );
+  return <LandingScene />;
 }
 
 export default App;
--- kripto-hans-client/src/components/LandingScene/LandingScene.tsx
+++ kripto-hans-client/src/components/LandingScene/LandingScene.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'components/LandingScene/LandingScene.scss';
 
 export const LandingScene: React.FC<Record<string, unknown>> = () => {
-  return <div className="landing-scene-container">AI NFT ART</div>;
+  return <div>AI NFT ART</div>;
 };
