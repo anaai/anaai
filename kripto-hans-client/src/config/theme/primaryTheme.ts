f9adee2b5ca272dd1cbc55abf683ce4ea3b5253d
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 23:39:33 +0200

    Enhance primary theme

--- kripto-hans-client/src/config/theme/primaryTheme.ts
+++ kripto-hans-client/src/config/theme/primaryTheme.ts
@@ -3,6 +3,7 @@ import { createTheme, responsiveFontSizes } from '@material-ui/core';
 // Theme palette: https://coolors.co/03256c-2541b2-1768ac-06bee1-ffffff
 export const primaryTheme = responsiveFontSizes(
   createTheme({
+    spacing: 16,
     typography: {
       fontFamily: 'Harmattan',
       fontSize: 32
@@ -12,7 +13,7 @@ export const primaryTheme = responsiveFontSizes(
         main: '#1768ac'
       },
       secondary: {
-        main: '#06BEE1'
+        main: '#24d6f9'
       }
     }
   })
