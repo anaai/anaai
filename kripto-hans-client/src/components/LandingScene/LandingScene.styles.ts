52bb530eec0258f4b0abdf0a6770dcb6c890abf7
Filip Kis <filip.kis.dev@gmail.com>
2021-08-14 21:48:01 +0200

    Add basic LandingScene styles

---
+++ kripto-hans-client/src/components/LandingScene/LandingScene.styles.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    height: '100vh'
+  }
+});
--- kripto-hans-client/src/components/LandingScene/LandingScene.tsx
+++ kripto-hans-client/src/components/LandingScene/LandingScene.tsx
@@ -1,5 +1,12 @@
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { useStyles } from './LandingScene.styles';
 
 export const LandingScene: React.FC<Record<string, unknown>> = () => {
-  return <div>AI NFT ART</div>;
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <Typography variant="h1">AI NFT ART</Typography>
+    </Box>
+  );
 };
