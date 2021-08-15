38a5d8de66859e00d82aae6df3c003ab88b1fe42
Filip Kis <filip.kis.dev@gmail.com>
2021-08-15 11:21:15 +0200

    Add LandingScene test cases

--- kripto-hans-client/src/components/LandingScene/LandingScene.test.tsx
+++ kripto-hans-client/src/components/LandingScene/LandingScene.test.tsx
@@ -6,3 +6,21 @@ test('renders ANA title', () => {
   const titleElement = screen.getByText(/ANA/i);
   expect(titleElement).toBeInTheDocument();
 });
+
+test('renders AI NFT Art subtitle', () => {
+  render(<LandingScene />);
+  const subtitleElement = screen.getByText(/AI NFT Art/i);
+  expect(subtitleElement).toBeInTheDocument();
+});
+
+test('renders GENERATE button', () => {
+  render(<LandingScene />);
+  const generateButton = screen.getByText(/Generate/i);
+  expect(generateButton).toBeInTheDocument();
+});
+
+test('renders EXPLORE button', () => {
+  render(<LandingScene />);
+  const exploreButton = screen.getByText(/Explore/i);
+  expect(exploreButton).toBeInTheDocument();
+});
