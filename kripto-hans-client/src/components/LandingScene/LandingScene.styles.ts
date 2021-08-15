81ccde964a0acf84b1006ab3a0e7c8ac9e7b3d99
Filip Kis <filip.kis.dev@gmail.com>
2021-08-15 02:15:59 +0200

    Enhance LandingScene UX

--- kripto-hans-client/src/components/LandingScene/LandingScene.styles.ts
+++ kripto-hans-client/src/components/LandingScene/LandingScene.styles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 export const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    height: '100vh',
+    minHeight: '100vh',
     background: `linear-gradient(200deg, #00555d88 0%, #18abc887 35%, #17ac7f88 100%), linear-gradient(160deg, #00555d88 0%, #18abc887 35%, #17ac7f88 100%)`,
     display: 'flex',
     justifyContent: 'center',
