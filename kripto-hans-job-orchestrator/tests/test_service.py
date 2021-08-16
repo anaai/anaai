2aa3faaacb65cb6c7b99140b2a8135409d64e85d
vonum <vonum.mk@gmail.com>
2021-08-16 12:36:10 +0200

    Mint nfts when job is finished

--- kripto-hans-job-orchestrator/tests/test_service.py
+++ kripto-hans-job-orchestrator/tests/test_service.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 from service import app
 
 client = TestClient(app)
@@ -9,7 +9,7 @@ def test_cartoonify_response(cartoonify_mock):
   task_id = 555333
   cartoonify_mock.delay.return_value.id = task_id
 
-  response = client.post("/cartoonify", json={"image_url": "", "image_name": ""})
+  response = client.post("/cartoonify", json={"image_url": "", "image_name": "", "payer": ""})
 
   assert response.status_code == 200
   assert response.json() == {"task_id": task_id}
@@ -18,8 +18,10 @@ def test_cartoonify_response(cartoonify_mock):
 def test_cartoonify_task_invocation(cartoonify_mock):
   url = "url"
   name = "name"
+  payer = "payer"
+  price = 0
   cartoonify_mock.delay.return_value.id = 555333
 
-  response = client.post("/cartoonify", json={"image_url": url, "image_name": name})
+  response = client.post("/cartoonify", json={"image_url": url, "image_name": name, "payer": payer})
 
-  cartoonify_mock.delay.assert_called_with(url, name)
+  cartoonify_mock.delay.assert_called_with(ANY, payer, price, url, name)
