8441754bacb80bd2cb69d93431a0d9cb2f74212a
Filip Kis <filip.kis.dev@gmail.com>
2021-08-18 00:24:32 +0200

    Add basic root routes

--- kripto-hans-client/src/components/RootView/RootView.tsx
+++ kripto-hans-client/src/components/RootView/RootView.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/no-children-prop */
 import { LandingScene } from 'components/LandingScene/LandingScene';
 import { createSetIsMetaMaskInstalledAction, useWallet } from 'contexts/WalletContext';
 import { useCallback, useEffect } from 'react';
+import { Redirect, Route, useLocation } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 
 export const RootView: React.FC<Record<string, unknown>> = () => {
   const { dispatch } = useWallet();
@@ -17,5 +20,12 @@ export const RootView: React.FC<Record<string, unknown>> = () => {
     initMetaMask();
   }, [initMetaMask]);
 
-  return <LandingScene />;
+  const location = useLocation();
+
+  return (
+    <AnimatedSwitch atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }}>
+      <Route exact path="/" component={LandingScene} />
+      <Route path="" render={() => <Redirect to="/" />} />
+    </AnimatedSwitch>
+  );
 };
