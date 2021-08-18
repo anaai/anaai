b97b27c77745e7ffa29ba641659f42091ae7374f
Filip Kis <filip.kis.dev@gmail.com>
2021-08-19 00:58:24 +0200

    Add basic GenerateScene input and validation

--- kripto-hans-client/src/components/GenerateScene/GenerateScene.styles.ts
+++ kripto-hans-client/src/components/GenerateScene/GenerateScene.styles.ts
@@ -1,11 +1,56 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles(() => ({
+export const useStyles = makeStyles((theme) => ({
   root: {
     flex: 1,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column'
+  },
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  urlForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageUrlInput: {
+    fontSize: theme.spacing(2),
+    fontFamily: 'inherit',
+    height: theme.spacing(4),
+    width: `calc(100vw - ${theme.spacing(4)}px)`,
+    maxWidth: theme.spacing(60),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    borderRadius: theme.spacing(2),
+    border: `${theme.spacing(0.2)}px solid ${theme.palette.primary.main}`,
+    '&:focus-visible': {
+      outline: 'none'
+    },
+    '&:focus': {
+      border: `${theme.spacing(0.2)}px solid ${theme.palette.secondary.main}`
+    }
+  },
+  urlErrorMessage: {
+    color: '#fff',
+    opacity: 0
+  },
+  urlErrorMessageVisible: {
+    opacity: 1
+  },
+  ctaButtonsContainer: {
+    marginTop: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    display: 'flex'
+  },
+  payGenerateButton: {
+    marginRight: theme.spacing(0.5)
+  },
+  backButton: {
+    marginLeft: theme.spacing(0.5)
   }
 }));
--- kripto-hans-client/src/components/GenerateScene/GenerateScene.tsx
+++ kripto-hans-client/src/components/GenerateScene/GenerateScene.tsx
@@ -1,6 +1,7 @@
-import { Box } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
 import { useWallet } from 'contexts/WalletContext';
-import { useState } from 'react';
+import { ChangeEventHandler, FocusEventHandler, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useStyles } from './GenerateScene.styles';
 
 export const GenerateScene: React.FC<Record<string, unknown>> = () => {
@@ -8,12 +9,40 @@ export const GenerateScene: React.FC<Record<string, unknown>> = () => {
     state: { contract, accounts }
   } = useWallet();
 
-  const [inputImageUrl, setInputImageUrl] = useState('');
+  const [url, setUrl] = useState('');
+  const [urlErrorMessage, setUrlErrorMessage] = useState('');
+  const [urlErrorShown, setUrlErrorShown] = useState(false);
+
+  const handleUrlChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const {
+      target: { value }
+    } = event;
+    setUrl(value);
+
+    const isUrlValid = validateUrl(value);
+
+    setUrlErrorMessage(isUrlValid ? '' : 'Invalid URL provided');
+  };
+  const handleUrlFocus: FocusEventHandler<HTMLInputElement> = (event) => event.target.select();
+
+  const handleUrlBlur: FocusEventHandler<HTMLInputElement> = () => setUrlErrorShown(true);
+
+  const validateUrl = (urlString: string) => {
+    let url;
+
+    try {
+      url = new URL(urlString);
+    } catch (_) {
+      return false;
+    }
+
+    return url.protocol === 'http:' || url.protocol === 'https:';
+  };
 
   const handlePayGenerating = async () => {
     if (contract) {
       const payGeneratingResult = await contract.methods
-        .payGenerating(inputImageUrl)
+        .payGenerating(url)
         .send({ from: accounts[0], gas: 1_000_000 });
 
       console.warn('payGeneratingResult: ', payGeneratingResult);
@@ -22,7 +51,64 @@ export const GenerateScene: React.FC<Record<string, unknown>> = () => {
     }
   };
 
+  const history = useHistory();
+
+  const handleBackClick = () => {
+    history.goBack();
+  };
+
   const classes = useStyles();
 
-  return <Box className={classes.root}>Generate scene works</Box>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.contentContainer}>
+        <form
+          className={classes.urlForm}
+          onSubmit={() => {
+            void 0;
+          }}
+        >
+          <input
+            className={classes.imageUrlInput}
+            type="url"
+            required
+            name="url"
+            value={url}
+            onChange={handleUrlChange}
+            onFocus={handleUrlFocus}
+            onBlur={handleUrlBlur}
+            placeholder="Add remote image url here"
+          />
+
+          <Typography
+            className={`${classes.urlErrorMessage} ${
+              urlErrorShown ? classes.urlErrorMessageVisible : ''
+            }`}
+            variant="body2"
+          >
+            {`${urlErrorMessage ? urlErrorMessage : 'Ready for Liftoff'} `}
+          </Typography>
+
+          <Box className={classes.ctaButtonsContainer}>
+            <Button
+              className={classes.payGenerateButton}
+              color="primary"
+              variant="contained"
+              onClick={handlePayGenerating}
+            >
+              Pay Image Generate
+            </Button>
+            <Button
+              className={classes.backButton}
+              color="secondary"
+              variant="contained"
+              onClick={handleBackClick}
+            >
+              Back
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
 };
