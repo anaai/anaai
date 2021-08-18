604cbc7f0d5513fafcef1e849103bcf1e0c54515
Filip Kis <filip.kis.dev@gmail.com>
2021-08-18 02:16:14 +0200

    Refactor WalletConnector

--- kripto-hans-client/src/components/WalletConnector/WalletConnector.tsx
+++ kripto-hans-client/src/components/WalletConnector/WalletConnector.tsx
@@ -38,51 +38,6 @@ export const WalletConnector: React.FC<Record<string, unknown>> = () => {
     metaMaskOnboarding.startOnboarding();
   };
 
-  const handleTransactionInit = async () => {
-    // eslint-disable-next-line no-debugger
-    // debugger;
-
-    const abi = StyleNFTContract.abi; // some ABI JSON;
-
-    console.warn(abi);
-
-    const address = process.env.REACT_APP_CONTRACT_ADDRESS; // some contract address as string;
-
-    console.warn(address);
-
-    const web3Instance = new Web3(window.ethereum);
-
-    console.warn(web3Instance);
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const myContract = new web3Instance.eth.Contract(abi as any, address);
-
-    console.warn(myContract);
-
-    myContract.events.TokenMinted(
-      { filter: { payer: accounts![0] } },
-      async (error: any, event: any) => {
-        console.warn('EVENT RECIEVED: ', event);
-      }
-    );
-
-    const something = await myContract.methods
-      .payGenerating(imageUrl)
-      .send({ from: accounts![0], gas: 1_000_000 });
-
-    console.warn(something);
-
-    alert(something.status);
-
-    // txHash is a hex string
-    // As with any RPC call, it may throw an error
-
-    // const txHash = await ethereum.request({
-    //   method: 'eth_sendTransaction',
-    //   params: [transactionParameters]
-    // });
-  };
-
   const classes = useStyles();
 
   const sharedButtonProps = {
@@ -92,15 +47,11 @@ export const WalletConnector: React.FC<Record<string, unknown>> = () => {
     'data-testid': 'metamask-button'
   };
 
-  const [imageUrl, setImageUrl] = useState('');
-
   return (
     <>
       {isMetaMaskInstalled ? (
         accounts.length ? (
-          <Button {...sharedButtonProps} onClick={handleTransactionInit}>
-            Connected
-          </Button>
+          <Button {...sharedButtonProps}>Connected</Button>
         ) : (
           <Button {...sharedButtonProps} onClick={handleConnectToMetaMask}>
             Connect
