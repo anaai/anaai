d251f0e0472b699c21b7d7372f59af160562edff
Filip Kis <filip.kis.dev@gmail.com>
2021-08-20 21:49:59 +0200

    Code cleanup

--- kripto-hans-client/src/components/RootView/RootView.tsx
+++ kripto-hans-client/src/components/RootView/RootView.tsx
@@ -1,28 +1,12 @@
 /* eslint-disable react/no-children-prop */
-import { useCallback, useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import { GenerateScene } from 'components/GenerateScene/GenerateScene';
 import { LandingScene } from 'components/LandingScene/LandingScene';
 import { WalletConnector } from 'components/WalletConnector/WalletConnector';
-import { createSetIsMetaMaskInstalledAction, useWallet } from 'contexts/WalletContext';
 import { useStyles } from './RootView.styles';
 
 export const RootView: React.FC<Record<string, unknown>> = () => {
-  const { dispatch } = useWallet();
-
-  const initMetaMask = useCallback(() => {
-    const { ethereum } = window;
-    const isMetaMaskInstalled = Boolean(ethereum && ethereum.isMetaMask);
-    if (isMetaMaskInstalled) {
-      dispatch(createSetIsMetaMaskInstalledAction(isMetaMaskInstalled));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    initMetaMask();
-  }, [initMetaMask]);
-
   const classes = useStyles();
 
   return (
