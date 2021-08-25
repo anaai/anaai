dbe10b32ea06ff8e3fe7748b32dab84792b771e1
vonum <vonum.mk@gmail.com>
2021-08-25 20:51:07 +0200

    Update script for local testing

--- job-orchestrator/scripts/trigger_job.sh
+++ job-orchestrator/scripts/trigger_job.sh
@@ -1,3 +1,3 @@
 curl -d '{"image_url": "https://ideascdn.lego.com/media/generate/lego_ci/dfda0c75-0eb4-4f5e-b859-d891088d3096/resize:950:633/legacy", "image_name": "test-image.jpeg", "payer": "0xc043945B556B526689270d87A7ac58B286F31308"}' \
   -H "Content-Type: application/json" \
-  -X POST http://ec2-18-219-110-34.us-east-2.compute.amazonaws.com:6000/cartoonify
+  -X POST http://localhost:8000/cartoonify
