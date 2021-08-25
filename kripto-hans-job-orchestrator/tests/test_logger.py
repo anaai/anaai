432279cc5db9b7db1e3653a02147a5dc03225a3f
vonum <vonum.mk@gmail.com>
2021-08-25 20:17:55 +0200

    Remove service from job orchestrator

--- kripto-hans-job-orchestrator/tests/test_logger.py
+++
@@ -1,9 +0,0 @@
-import logger
-
-def test_log_job_request(logs):
-  logger.log_job_request("payer", "image", "url")
-  assert "Recieved job request from payer for image at url" in logs.info
-
-def test_log_job_started(logs):
-  logger.log_job_started(13)
-  assert "Started job 13." in logs.info
