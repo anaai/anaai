4c6b06c8495fb170515a98bc3e01f4de82a9a167
vonum <vonum.mk@gmail.com>
2021-08-25 20:37:01 +0200

    Rename nft-service

--- kripto-hans-service/scripts/transfer_token.sh
+++
@@ -1,3 +0,0 @@
-curl -d '{"recipient": "0xc043945B556B526689270d87A7ac58B286F31308", "token_id": 1, "price": 0}'\
-  -H "Content-Type: application/json"\
-  -X POST http://localhost:8000/transfer_token
