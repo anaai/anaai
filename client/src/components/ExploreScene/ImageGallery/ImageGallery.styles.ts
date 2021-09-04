424c3af9d5b864b7225377d6713961002133eed2
Filip Kis <filip.kis.dev@gmail.com>
2021-09-04 21:13:36 +0200

    Code cleanup

--- client/src/components/ExploreScene/ImageGallery/ImageGallery.styles.ts
+++ client/src/components/ExploreScene/ImageGallery/ImageGallery.styles.ts
@@ -4,9 +4,7 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     flex: 1,
     display: 'flex',
-    // justifyContent: 'center',
     alignItems: 'center',
-    // flexDirection: 'column',
     flexWrap: 'wrap'
   },
   imageContainer: {
