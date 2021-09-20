b0840757bc8f51528494c205a1047f464ede6593
vonum <vonum.mk@gmail.com>
2021-09-20 14:18:20 +0200

    Refactor rest of tests

--- contract/test/Tokens.test.js
+++ contract/test/Tokens.test.js
@@ -6,7 +6,6 @@ const StyleNFT = contract.fromArtifact("StyleNFT");
 
 describe("StyleNFT", () => {
   const [ owner, user1 ] = accounts;
-  const price = web3.utils.toWei("1", "ether");
   const uri = "token1URI";
 
   beforeEach(async () => {
@@ -19,7 +18,7 @@ describe("StyleNFT", () => {
       const oldSupply = await this.contract.totalSupply();
       const oldBalance = await this.contract.balanceOf(owner);
 
-      await this.contract.mintNFT(owner, user1, uri, price, {from: owner});
+      await this.contract.mintNFT(owner, user1, uri, {from: owner});
 
       const tokenOwner = await this.contract.ownerOf(1);
       const tokenURI = await this.contract.tokenURI(1);
@@ -40,61 +39,19 @@ describe("StyleNFT", () => {
     it("Emits TokenMinted event", async () => {
       const tokenId = new BN("1");
 
-      const tx = await this.contract.mintNFT(owner, user1, uri, price, {from: owner});
+      const tx = await this.contract.mintNFT(owner, user1, uri, {from: owner});
 
       expectEvent(
         tx,
         "TokenMinted",
-        {recipient: owner, payer: user1, tokenId, tokenURI: uri, price}
+        {recipient: owner, payer: user1, tokenId, tokenURI: uri}
       );
     });
 
     it("Doesn't mint a token when you are not the owner", async () => {
       await expectRevert(
-        this.contract.mintNFT(user1, user1, uri, price, {from: user1}),
+        this.contract.mintNFT(user1, user1, uri, {from: user1}),
         "Ownable: caller is not the owner"
-      );
-    });
-  });
-
-  describe("updateTokenPrice", () => {
-    it("Changes the price of the token", async () => {
-      const tokenId = new BN("1");
-      const newPrice = web3.utils.toWei("2", "ether");
-
-      await this.contract.mintNFT(owner, user1, uri, price, {from: owner});
-      await this.contract.updateTokenPrice(tokenId, newPrice, {from: owner});
-
-      await expectRevert(
-        this.contract.payImage(tokenId, {from: user1, value: price}),
-        "Transaction value must match nft price"
-      );
-
-      tx = await this.contract.contract.methods
-        .payImage(tokenId)
-        .send({from: user1, value: newPrice, gas: 500000});
-
-      expect(tx.status).to.equal(true);
-    });
-
-    it("Reverts when token doesn't exist", async () => {
-      const tokenId = new BN("1");
-
-      await expectRevert(
-        this.contract.updateTokenPrice(tokenId, price, {from: owner}),
-        "Token does not exist"
-      );
-    });
-
-    it("Reverts when token is already bought", async () => {
-      const tokenId = new BN("1");
-
-      await this.contract.mintNFT(owner, user1, uri, price, {from: owner});
-      await this.contract.payImage(tokenId, {from: user1, value: price});
-
-      await expectRevert(
-        this.contract.updateTokenPrice(tokenId, price, {from: owner}),
-        "Token already bought"
       );
     });
   });
