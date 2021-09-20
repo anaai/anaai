17b16c28625b4f21fd3b6826bfda0f61aa125a97
vonum <vonum.mk@gmail.com>
2021-09-20 15:33:24 +0200

    Update mintNFT and remove safe transfer endpoint from in nft service

--- nft-service/logger.py
+++ nft-service/logger.py
@@ -7,10 +7,6 @@ def log_token_minted(token_id):
   logger = _logger()
   logger.info(f"Token minted with id: {token_id}.")
 
-def log_token_transfered(token_id, sender, recipient):
-  logger = _logger()
-  logger.info(f"Token with id: {token_id} transfered from {sender} to {recipient}.")
-
 def _logger():
   logging.basicConfig(format=FORMAT, level=logging.INFO)
   return logging.getLogger(LOGGER_NAME)
--- nft-service/service.py
+++ nft-service/service.py
@@ -21,15 +21,8 @@ CONTRACT_PATH = "contracts/StyleNFT.json"
 app = FastAPI()
 
 class NFTPayload(BaseModel):
-  recipient: str
   payer: str
   token_uri: str
-  price: float
-
-class TransferTokenPayload(BaseModel):
-  recipient: str
-  token_id: int
-  price: float
 
 @app.get("/status")
 async def get_status():
@@ -44,15 +37,3 @@ async def mint_nft(nft: NFTPayload):
                                Web3.toWei(nft.price, "ether"))
   logger.log_token_minted(token_id)
   return JSONResponse({"token_id": token_id})
-
-@app.post("/transfer_token")
-async def transfer_token(transfer: TransferTokenPayload):
-  contract = style_nft_factory(PUBLIC_KEY, PRIVATE_KEY, API_URL, CONTRACT_ADDRESS, CONTRACT_PATH)
-  receipt_args = contract.safe_transfer_from(Web3.toChecksumAddress(transfer.recipient),
-                                             transfer.token_id)
-  logger.log_token_transfered(
-    receipt_args["tokenId"],
-    receipt_args["from"],
-    receipt_args["to"]
-  )
-  return JSONResponse(receipt_args.__dict__)
