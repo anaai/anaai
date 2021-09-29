3e151fb9eee5ef3b6bd22f3ea33660661a09ed0d
Filip Kis <filip.kis.dev@gmail.com>
2021-09-30 00:17:14 +0200

    Add TransformationDetailsScene styles

--- client/src/components/TransformationDetailsScene/TransformationDetailsScene.styles.ts
+++ client/src/components/TransformationDetailsScene/TransformationDetailsScene.styles.ts
@@ -1,11 +1,81 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles(() => ({
+export const useStyles = makeStyles((theme) => ({
   root: {
     flex: 1,
     display: 'flex',
-    justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column'
+  },
+  tranformationCard: {
+    borderRadius: theme.spacing(2),
+    backgroundColor: 'rgba(255, 255, 255, .15)',
+    backdropFilter: 'blur(5px)',
+    display: 'flex',
+    margin: theme.spacing(5),
+    color: '#fff',
+    textShadow: '1px 1px #0005',
+    cursor: 'pointer',
+    userSelect: 'none',
+    overflow: 'hidden',
+    transition: theme.transitions.create('background-color', {
+      duration: theme.transitions.duration.short,
+      easing: theme.transitions.easing.easeInOut
+    }),
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, .20)'
+    }
+  },
+  transformationInfoContainer: {
+    flex: 1,
+    maxWidth: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(2)
+  },
+  transformationTitle: {
+    marginBottom: theme.spacing(1)
+  },
+  transformationDescription: {
+    marginBottom: theme.spacing(2),
+    flex: 1
+  },
+  transformationStats: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  transformationStat: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(1),
+    textAlign: 'center'
+  },
+  transformationStatValue: {
+    marginBottom: theme.spacing(1)
+  },
+  transformationImagesContainer: {
+    flex: 1,
+    maxWidth: '50%',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridTemplateRows: '1fr 1fr',
+    columnGap: theme.spacing(1),
+    rowGap: theme.spacing(1),
+    padding: theme.spacing(4),
+    backgroundColor: '#fff',
+    transition: `${theme.transitions.create(['padding'], {
+      duration: theme.transitions.duration.short,
+      easing: theme.transitions.easing.easeInOut
+    })} `,
+    '&:hover': {
+      padding: theme.spacing(3)
+    }
+  },
+  tranformationImage: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
   }
 }));
--- client/src/components/TransformationDetailsScene/TransformationDetailsScene.tsx
+++ client/src/components/TransformationDetailsScene/TransformationDetailsScene.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { useParams } from 'react-router';
 import { useStyles } from './TransformationDetailsScene.styles';
 
@@ -9,7 +9,65 @@ export const TransformationDetailsScene: React.FC<Record<string, unknown>> = () 
 
   return (
     <Box className={classes.root} data-testid="TransformationDetailsScene-root-container">
-      Transformation Details Scene works {id}
+      <Box className={classes.tranformationCard}>
+        <Box className={classes.transformationInfoContainer}>
+          <Typography className={classes.transformationTitle} variant="h5">
+            Transformation title
+          </Typography>
+
+          <Typography className={classes.transformationDescription} variant="body2">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis iure ipsa
+            necessitatibus provident nam quaerat quas. Maiores consequuntur iusto, temporibus quam
+            beatae culpa libero dicta quidem repellat earum repellendus soluta!
+          </Typography>
+
+          <Box className={classes.transformationStats}>
+            <Box className={classes.transformationStat}>
+              <Typography className={classes.transformationStatValue} variant="h6">
+                15
+              </Typography>
+              <Typography>Lorem start</Typography>
+            </Box>
+
+            <Box className={classes.transformationStat}>
+              <Typography className={classes.transformationStatValue} variant="h6">
+                15
+              </Typography>
+              <Typography>Lorem start</Typography>
+            </Box>
+
+            <Box className={classes.transformationStat}>
+              <Typography className={classes.transformationStatValue} variant="h6">
+                15
+              </Typography>
+              <Typography>Lorem start</Typography>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box className={classes.transformationImagesContainer}>
+          <img
+            className={classes.tranformationImage}
+            src="https://trello.com/1/cards/614765dca3613935ccdc0650/attachments/615335823572a21f5a51ee0f/previews/615335823572a21f5a51ee55/download/lake.jpeg.jpg"
+            alt="example"
+          />
+          <img
+            className={classes.tranformationImage}
+            src="https://trello.com/1/cards/614765dca3613935ccdc0650/attachments/6153357c9744274e1150b8cd/previews/6153357e9744274e1150b901/download/gril.jpeg.jpg"
+            alt="example"
+          />
+          <img
+            className={classes.tranformationImage}
+            src="https://trello.com/1/cards/614765dca3613935ccdc0650/attachments/61533574ebd5aa595af3b47a/previews/61533575ebd5aa595af3b4be/download/nature.jpeg.jpg"
+            alt="example"
+          />
+          <img
+            className={classes.tranformationImage}
+            src="https://trello.com/1/cards/614765dca3613935ccdc0650/attachments/61533570863d532acf250fce/previews/61533571863d532acf250fe7/download/dogo.jpeg.jpg"
+            alt="example"
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
