e1baf11831c63a06462aa767750c4fa6c98fffcd
vonum <vonum.mk@gmail.com>
2021-09-29 18:25:59 +0200

    Update nft-service scripts

--- nft-service/scripts/mint_nft.sh
+++ nft-service/scripts/mint_nft.sh
@@ -1,3 +1,3 @@
-curl -d '{"recipient": "0xc043945B556B526689270d87A7ac58B286F31308", "payer": "0xc043945B556B526689270d87A7ac58B286F31308", "token_uri": "https://gateway.pinata.cloud/ipfs/QmTLJSa7Pce19CHhKgSZoSbHtQPtBjVSHvhvR4bCKbbYUs", "price": 0}'\
+curl -d '{"payer": "0xc043945B556B526689270d87A7ac58B286F31308", "token_uri": "https://gateway.pinata.cloud/ipfs/QmTLJSa7Pce19CHhKgSZoSbHtQPtBjVSHvhvR4bCKbbYUs"}'\
   -H "Content-Type: application/json"\
-  -X POST http://localhost:8000/mint_nft
+  -X POST http://localhost:7000/mint_nft
