7d7375b2bddd8443272504a11c263e439bf5d36f
Filip Kis <filip.kis.dev@gmail.com>
2021-10-04 20:40:33 +0200

    Add transformations

--- client/src/config/transformations/transformations.ts
+++ client/src/config/transformations/transformations.ts
@@ -1,7 +1,13 @@
-export const transformationIds = [
+import { images } from 'config/imageLoader/imageLoader';
+
+export const transformationNames = [
   'ascii',
   'candy',
   'cartoonification',
   'feathers',
   'sketch'
 ] as const;
+
+export type TransformationName = typeof transformationNames[number];
+
+export type ImageName = keyof typeof images[TransformationName];
