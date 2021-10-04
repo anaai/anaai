e874f85606ef5dd6d45ac1c70c9c01cc124aae07
Filip Kis <filip.kis.dev@gmail.com>
2021-10-04 20:43:28 +0200

    Add TransformationCard mocked ui

--- client/src/components/TransformationCard/TransformationCard.styles.ts
+++ client/src/components/TransformationCard/TransformationCard.styles.ts
@@ -1,11 +1,89 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles(() => ({
+export const useStyles = makeStyles((theme) => ({
   root: {
     flex: 1,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    maxWidth: '100%'
+  },
+  tranformationCard: {
+    borderRadius: theme.spacing(0.2),
+    backgroundColor: 'rgba(255, 255, 255, .15)',
+    backdropFilter: 'blur(5px)',
+    display: 'flex',
+    margin: `${theme.spacing(2)}px ${theme.spacing(5)}px`,
+    userSelect: 'none',
+    overflow: 'hidden',
+    width: theme.spacing(100),
+    maxWidth: `calc(100% - ${theme.spacing(10)}px)`,
+    boxShadow: theme.shadows[1],
+    transition: theme.transitions.create('background-color', {
+      duration: theme.transitions.duration.short,
+      easing: theme.transitions.easing.easeInOut
+    }),
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, .20)'
+    },
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse'
+    }
+  },
+  transformationInfoContainer: {
+    flex: 1,
+    maxWidth: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: 'initial'
+    }
+  },
+  transformationTitle: {
+    marginBottom: theme.spacing(1)
+  },
+  transformationDescription: {
+    marginBottom: theme.spacing(2),
+    flex: 1
+  },
+  transformationStats: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  transformationStat: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(1),
+    textAlign: 'center'
+  },
+  transformationStatValue: {},
+  transformationCardActionsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: theme.spacing(2)
+  },
+  generateNowButton: { marginRight: theme.spacing(2) },
+  learnMoreButton: {},
+  transformationImagesContainer: {
+    flex: 1,
+    maxWidth: '50%',
+    '& > div': {
+      height: '100%!important'
+    },
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: 'initial',
+      '& > div': {
+        height: '400px!important'
+      }
+    }
+  },
+  tranformationImage: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
   }
 }));
--- client/src/components/TransformationCard/TransformationCard.tsx
+++ client/src/components/TransformationCard/TransformationCard.tsx
@@ -1,12 +1,94 @@
-import { Box } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
+import { images } from 'config/imageLoader/imageLoader';
+import { transformationNames } from 'config/transformations/transformations';
+import ReactCompareImage from 'react-compare-image';
+import { useHistory, useLocation } from 'react-router';
 import { useStyles } from './TransformationCard.styles';
 
-export const TransformationCard: React.FC<Record<string, unknown>> = () => {
+interface TransformationCardProps {
+  transformationName: typeof transformationNames[number];
+}
+
+export const TransformationCard: React.FC<TransformationCardProps> = ({ transformationName }) => {
   const classes = useStyles();
+
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleLearnMoreClick = () => {
+    history.push(`${location.pathname}/${transformationName}`);
+  };
+
+  const handleGenerateClick = () => {
+    history.push(`/generate/${transformationName}`);
+  };
 
   return (
     <Box className={classes.root} data-testid="TransformationCard-root-container">
-      Transformation Card works
+      <Box className={classes.tranformationCard}>
+        <Box className={classes.transformationInfoContainer}>
+          <Typography className={classes.transformationTitle} variant="h5">
+            Transformation title
+          </Typography>
+
+          <Typography className={classes.transformationDescription} variant="body2">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis iure ipsa
+            necessitatibus provident nam quaerat quas. Maiores consequuntur iusto, temporibus quam
+            beatae culpa libero dicta quidem repellat earum repellendus soluta!
+          </Typography>
+
+          <Box className={classes.transformationStats}>
+            <Box className={classes.transformationStat}>
+              <Typography className={classes.transformationStatValue} variant="h6">
+                15
+              </Typography>
+              <Typography>Lorem start</Typography>
+            </Box>
+
+            <Box className={classes.transformationStat}>
+              <Typography className={classes.transformationStatValue} variant="h6">
+                15
+              </Typography>
+              <Typography>Lorem start</Typography>
+            </Box>
+
+            <Box className={classes.transformationStat}>
+              <Typography className={classes.transformationStatValue} variant="h6">
+                15
+              </Typography>
+              <Typography>Lorem start</Typography>
+            </Box>
+          </Box>
+
+          <Box className={classes.transformationCardActionsContainer}>
+            <Button
+              onClick={handleGenerateClick}
+              className={classes.generateNowButton}
+              color="primary"
+              variant="contained"
+            >
+              Generate
+            </Button>
+
+            <Button
+              onClick={handleLearnMoreClick}
+              className={classes.learnMoreButton}
+              color="secondary"
+              variant="contained"
+            >
+              Learn more
+            </Button>
+          </Box>
+        </Box>
+
+        <Box className={classes.transformationImagesContainer}>
+          <ReactCompareImage
+            sliderPositionPercentage={0.33}
+            leftImage={images[transformationName].fGirl}
+            rightImage={images.base.fGirl}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
