bd66c2453e3a787fdee1591e6eb7b22b1fc4ec12
Filip Kis <filip.kis.dev@gmail.com>
2021-10-04 20:44:56 +0200

    Enhance GenerateScene test

--- client/src/components/GenerateScene/GenerateScene.test.tsx
+++ client/src/components/GenerateScene/GenerateScene.test.tsx
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import { GenerateScene } from './GenerateScene';
+import { createMemoryHistory } from 'history';
+import { Router, Route } from 'react-router-dom';
 
-test('renders generate button', () => {
-  render(<GenerateScene />);
-  const generateButton = screen.getByText(/generate/i);
-  expect(generateButton).toBeInTheDocument();
+beforeEach(() => {
+  const history = createMemoryHistory();
+  const route = 'generate/sketch';
+  history.push(route);
+
+  render(
+    <Router history={history}>
+      <Route path="/generate/:transformationName" component={GenerateScene} />
+    </Router>
+  );
+});
+
+test('renders generate scene root container', () => {
+  const rootContainerElement = screen.getByTestId(/GenerateScene-root-container/i);
+  expect(rootContainerElement).toBeInTheDocument();
 });
