126a9681f4528137b1b09ab3c490ec7af6bf77e6
Filip Kis <filip.kis.dev@gmail.com>
2021-10-04 20:38:13 +0200

    Add new images to imageLoader

--- client/src/config/imageLoader/imageLoader.ts
+++ client/src/config/imageLoader/imageLoader.ts
@@ -1,58 +1,152 @@
-import baseDogo from 'assets/images/transformations/base/dogo.jpg';
-import baseGril from 'assets/images/transformations/base/gril.jpg';
-import baseLake from 'assets/images/transformations/base/lake.jpg';
-import baseNature from 'assets/images/transformations/base/nature.jpg';
+import baseFGirl from 'assets/images/transformations/base/fgirl.jpg';
+import baseFCat from 'assets/images/transformations/base/fcat.jpg';
+import baseFDog from 'assets/images/transformations/base/fdog.jpg';
+import baseFParrot from 'assets/images/transformations/base/fparrot.jpg';
+import baseFBeach from 'assets/images/transformations/base/fbeach.jpg';
+import baseFSky from 'assets/images/transformations/base/fsky.jpg';
+import baseMGirl from 'assets/images/transformations/base/mgirl.jpg';
+import baseMDogs from 'assets/images/transformations/base/mdogs.jpg';
+import baseMLake from 'assets/images/transformations/base/mlake.jpg';
+import baseMForest from 'assets/images/transformations/base/mforest.jpg';
+import baseMCity from 'assets/images/transformations/base/mcity.jpg';
 
-import candyDogo from 'assets/images/transformations/candy/dogo.jpg';
-import candyGril from 'assets/images/transformations/candy/gril.jpg';
-import candyLake from 'assets/images/transformations/candy/lake.jpg';
-import candyNature from 'assets/images/transformations/candy/nature.jpg';
+import cartoonificationFGirl from 'assets/images/transformations/cartoonification/fgirl-cartoonify.jpg';
+import cartoonificationFCat from 'assets/images/transformations/cartoonification/fcat-cartoonify.jpg';
+import cartoonificationFDog from 'assets/images/transformations/cartoonification/fdog-cartoonify.jpg';
+import cartoonificationFParrot from 'assets/images/transformations/cartoonification/fparrot-cartoonify.jpg';
+import cartoonificationFBeach from 'assets/images/transformations/cartoonification/fbeach-cartoonify.jpg';
+import cartoonificationFSky from 'assets/images/transformations/cartoonification/fsky-cartoonify.jpg';
+import cartoonificationMGirl from 'assets/images/transformations/cartoonification/mgirl-cartoonify.jpg';
+import cartoonificationMDogs from 'assets/images/transformations/cartoonification/mdogs-cartoonify.jpg';
+import cartoonificationMLake from 'assets/images/transformations/cartoonification/mlake-cartoonify.jpg';
+import cartoonificationMForest from 'assets/images/transformations/cartoonification/mforest-cartoonify.jpg';
+import cartoonificationMCity from 'assets/images/transformations/cartoonification/mcity-cartoonify.jpg';
 
-import feathersDogo from 'assets/images/transformations/feathers/dogo.jpg';
-import feathersGril from 'assets/images/transformations/feathers/gril.jpg';
-import feathersLake from 'assets/images/transformations/feathers/lake.jpg';
-import feathersNature from 'assets/images/transformations/feathers/nature.jpg';
+import asciiFGirl from 'assets/images/transformations/ascii/fgirl-ascii.jpg';
+import asciiFCat from 'assets/images/transformations/ascii/fcat-ascii.jpg';
+import asciiFDog from 'assets/images/transformations/ascii/fdog-ascii.jpg';
+import asciiFParrot from 'assets/images/transformations/ascii/fparrot-ascii.jpg';
+import asciiFBeach from 'assets/images/transformations/ascii/fbeach-ascii.jpg';
+import asciiFSky from 'assets/images/transformations/ascii/fsky-ascii.jpg';
+import asciiMGirl from 'assets/images/transformations/ascii/mgirl-ascii.jpg';
+import asciiMDogs from 'assets/images/transformations/ascii/mdogs-ascii.jpg';
+import asciiMLake from 'assets/images/transformations/ascii/mlake-ascii.jpg';
+import asciiMForest from 'assets/images/transformations/ascii/mforest-ascii.jpg';
+import asciiMCity from 'assets/images/transformations/ascii/mcity-ascii.jpg';
 
-import sketchDogo from 'assets/images/transformations/sketch/dogo.jpg';
-import sketchGril from 'assets/images/transformations/sketch/gril.jpg';
-import sketchLake from 'assets/images/transformations/sketch/lake.jpg';
-import sketchNature from 'assets/images/transformations/sketch/nature.jpg';
+import sketchFGirl from 'assets/images/transformations/sketch/fgirl-sketch.jpg';
+import sketchFCat from 'assets/images/transformations/sketch/fcat-sketch.jpg';
+import sketchFDog from 'assets/images/transformations/sketch/fdog-sketch.jpg';
+import sketchFParrot from 'assets/images/transformations/sketch/fparrot-sketch.jpg';
+import sketchFBeach from 'assets/images/transformations/sketch/fbeach-sketch.jpg';
+import sketchFSky from 'assets/images/transformations/sketch/fsky-sketch.jpg';
+import sketchMGirl from 'assets/images/transformations/sketch/mgirl-sketch.jpg';
+import sketchMDogs from 'assets/images/transformations/sketch/mdogs-sketch.jpg';
+import sketchMLake from 'assets/images/transformations/sketch/mlake-sketch.jpg';
+import sketchMForest from 'assets/images/transformations/sketch/mforest-sketch.jpg';
+import sketchMCity from 'assets/images/transformations/sketch/mcity-sketch.jpg';
+
+import candyFGirl from 'assets/images/transformations/candy/fgirl-candy.jpg';
+import candyFCat from 'assets/images/transformations/candy/fcat-candy.jpg';
+import candyFDog from 'assets/images/transformations/candy/fdog-candy.jpg';
+import candyFParrot from 'assets/images/transformations/candy/fparrot-candy.jpg';
+import candyFBeach from 'assets/images/transformations/candy/fbeach-candy.jpg';
+import candyFSky from 'assets/images/transformations/candy/fsky-candy.jpg';
+import candyMGirl from 'assets/images/transformations/candy/mgirl-candy.jpg';
+import candyMDogs from 'assets/images/transformations/candy/mdogs-candy.jpg';
+import candyMLake from 'assets/images/transformations/candy/mlake-candy.jpg';
+import candyMForest from 'assets/images/transformations/candy/mforest-candy.jpg';
+import candyMCity from 'assets/images/transformations/candy/mcity-candy.jpg';
+
+import feathersFGirl from 'assets/images/transformations/feathers/fgirl-feathers.jpg';
+import feathersFCat from 'assets/images/transformations/feathers/fcat-feathers.jpg';
+import feathersFDog from 'assets/images/transformations/feathers/fdog-feathers.jpg';
+import feathersFParrot from 'assets/images/transformations/feathers/fparrot-feathers.jpg';
+import feathersFBeach from 'assets/images/transformations/feathers/fbeach-feathers.jpg';
+import feathersFSky from 'assets/images/transformations/feathers/fsky-feathers.jpg';
+import feathersMGirl from 'assets/images/transformations/feathers/mgirl-feathers.jpg';
+import feathersMDogs from 'assets/images/transformations/feathers/mdogs-feathers.jpg';
+import feathersMLake from 'assets/images/transformations/feathers/mlake-feathers.jpg';
+import feathersMForest from 'assets/images/transformations/feathers/mforest-feathers.jpg';
+import feathersMCity from 'assets/images/transformations/feathers/mcity-feathers.jpg';
 
 export const images = {
   base: {
-    dogo: baseDogo,
-    gril: baseGril,
-    lake: baseLake,
-    nature: baseNature
-  },
-  candy: {
-    dogo: candyDogo,
-    gril: candyGril,
-    lake: candyLake,
-    nature: candyNature
-  },
-  feathers: {
-    dogo: feathersDogo,
-    gril: feathersGril,
-    lake: feathersLake,
-    nature: feathersNature
-  },
-  sketch: {
-    dogo: sketchDogo,
-    gril: sketchGril,
-    lake: sketchLake,
-    nature: sketchNature
-  },
-  ascii: {
-    dogo: sketchDogo,
-    gril: sketchGril,
-    lake: sketchLake,
-    nature: sketchNature
+    fGirl: baseFGirl,
+    fCat: baseFCat,
+    fDog: baseFDog,
+    fParrot: baseFParrot,
+    fBeach: baseFBeach,
+    fSky: baseFSky,
+    mGirl: baseMGirl,
+    mDogs: baseMDogs,
+    mLake: baseMLake,
+    mForest: baseMForest,
+    mCity: baseMCity
   },
   cartoonification: {
-    dogo: sketchDogo,
-    gril: sketchGril,
-    lake: sketchLake,
-    nature: sketchNature
+    fGirl: cartoonificationFGirl,
+    fCat: cartoonificationFCat,
+    fDog: cartoonificationFDog,
+    fParrot: cartoonificationFParrot,
+    fBeach: cartoonificationFBeach,
+    fSky: cartoonificationFSky,
+    mGirl: cartoonificationMGirl,
+    mDogs: cartoonificationMDogs,
+    mLake: cartoonificationMLake,
+    mForest: cartoonificationMForest,
+    mCity: cartoonificationMCity
+  },
+  ascii: {
+    fGirl: asciiFGirl,
+    fCat: asciiFCat,
+    fDog: asciiFDog,
+    fParrot: asciiFParrot,
+    fBeach: asciiFBeach,
+    fSky: asciiFSky,
+    mGirl: asciiMGirl,
+    mDogs: asciiMDogs,
+    mLake: asciiMLake,
+    mForest: asciiMForest,
+    mCity: asciiMCity
+  },
+  sketch: {
+    fGirl: sketchFGirl,
+    fCat: sketchFCat,
+    fDog: sketchFDog,
+    fParrot: sketchFParrot,
+    fBeach: sketchFBeach,
+    fSky: sketchFSky,
+    mGirl: sketchMGirl,
+    mDogs: sketchMDogs,
+    mLake: sketchMLake,
+    mForest: sketchMForest,
+    mCity: sketchMCity
+  },
+  candy: {
+    fGirl: candyFGirl,
+    fCat: candyFCat,
+    fDog: candyFDog,
+    fParrot: candyFParrot,
+    fBeach: candyFBeach,
+    fSky: candyFSky,
+    mGirl: candyMGirl,
+    mDogs: candyMDogs,
+    mLake: candyMLake,
+    mForest: candyMForest,
+    mCity: candyMCity
+  },
+  feathers: {
+    fGirl: feathersFGirl,
+    fCat: feathersFCat,
+    fDog: feathersFDog,
+    fParrot: feathersFParrot,
+    fBeach: feathersFBeach,
+    fSky: feathersFSky,
+    mGirl: feathersMGirl,
+    mDogs: feathersMDogs,
+    mLake: feathersMLake,
+    mForest: feathersMForest,
+    mCity: feathersMCity
   }
 } as const;
