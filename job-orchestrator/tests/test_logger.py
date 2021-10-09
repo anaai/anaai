9cb630a14dc89451e646ad2dbc03f0c64b77d6a0
vonum <vonum.mk@gmail.com>
2021-10-09 12:28:36 +0200

    Change token name in token metadata

--- job-orchestrator/tests/test_logger.py
+++ job-orchestrator/tests/test_logger.py
@@ -5,8 +5,8 @@ def test_log_image_downloaded(logs):
   assert "Image downloaded from http://image_url.com." in logs.info
 
 def test_log_image_generated(logs):
-  logger.log_image_generated("style transfer")
-  assert "Image generated using style transfer transformation" in logs.info
+  logger.log_image_generated("style transfer", 1)
+  assert "Image generated using style transfer transformation #1" in logs.info
 
 def test_log_image_uploaded(logs):
   logger.log_image_uploaded("ipfs://image_url.com")
