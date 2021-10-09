bc6787372183a09c4d1db5d4dceb99a066f9f5fd
vonum <vonum.mk@gmail.com>
2021-10-09 12:56:36 +0200

    Update contract abi in nft service

--- job-service/crud.py
+++ job-service/crud.py
@@ -5,11 +5,11 @@ import models
 def create_job_request(session, job_request):
   db_job_request = models.JobRequest(
     job_request_hash=job_request.image_name,
-     transformation=job_request.transformation,
-     transformation_number=job_request.transformation_number,
-     transaction_hash=job_request.transaction_hash,
-     block_hash=job_request.block_hash,
-     payer=job_request.payer
+    transformation=job_request.transformation,
+    transformation_number=job_request.transformation_number,
+    transaction_hash=job_request.transaction_hash,
+    block_hash=job_request.block_hash,
+    payer=job_request.payer
   )
   session.add(db_job_request)
   session.commit()
