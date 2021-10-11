3ffdeacf2ab11947239ea304e98eca9584e807b6
Filip Kis <filip.kis.dev@gmail.com>
2021-10-11 23:10:58 +0200

    Add MainSection boilerplate

--- landing-page/src/components/MainSection/MainSection.styles.ts
+++ landing-page/src/components/MainSection/MainSection.styles.ts
@@ -15,5 +15,35 @@ export const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     margin: `${theme.spacing(0.5)}px auto`,
     maxWidth: theme.spacing(100)
+  },
+  heroSectionContainer: {
+    display: 'flex'
+  },
+  heroTextContainer: {
+    flex: 1
+  },
+  heroTitle: {
+    color: '#fff',
+    userSelect: 'none',
+    letterSpacing: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    zIndex: 1,
+    textShadow: '0 3px 3px #0005',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  heroSubtitle: {
+    color: '#fff',
+    userSelect: 'none',
+    letterSpacing: theme.spacing(1.5),
+    paddingLeft: theme.spacing(2),
+    zIndex: 1,
+    textShadow: '0 3px 3px #0005',
+    marginBottom: theme.spacing(2),
+    textAlign: 'center'
+  },
+  heroDescription: {},
+  heroImageContainer: {
+    flex: 1
   }
 }));
--- landing-page/src/components/MainSection/MainSection.tsx
+++ landing-page/src/components/MainSection/MainSection.tsx
@@ -6,7 +6,25 @@ export const MainSection: React.FC<Record<string, unknown>> = () => {
 
   return (
     <Box className={classes.root} data-testid="MainSection-root-container">
-      <Box className={classes.mainSectionContentContainer}>Main section works</Box>
+      <Box className={classes.mainSectionContentContainer}>
+        <Box className={classes.heroSectionContainer}>
+          <Box className={classes.heroTextContainer}>
+            <Typography variant="h3" className={classes.heroTitle}>
+              ANA
+            </Typography>
+            <Typography variant="body1" className={classes.heroSubtitle}>
+              AI GENERATED ART
+            </Typography>
+            <Typography variant="body1" className={classes.heroDescription}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde, enim illo ea
+              velit maiores nihil ipsum. Doloremque amet perspiciatis velit nihil, illum officia
+              illo odio quo laudantium ea reprehenderit.
+            </Typography>
+          </Box>
+
+          <Box className={classes.heroImageContainer}>image placeholder</Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
