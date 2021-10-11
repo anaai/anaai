8e915c1ea22ba4150bf766e99bf7bfad9f24e3ba
Filip Kis <filip.kis.dev@gmail.com>
2021-10-11 23:28:39 +0200

    Add ResizeObserver mock to tests

--- landing-page/src/components/App.test.tsx
+++ landing-page/src/components/App.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from 'components/App';
+import { injectGlobalResizeObserverMock } from 'tests/mocks/ResizeObserver';
 
 test('no 🦃 in sight', () => {
+  injectGlobalResizeObserverMock();
   render(<App />);
   const turkeyElement = screen.queryByText(/🦃/i);
   expect(turkeyElement).not.toBeInTheDocument();
