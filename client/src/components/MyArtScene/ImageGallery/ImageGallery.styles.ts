93441dc0fac7b84803d30eaaac34fd353a0ee22d
Filip Kis <filip.kis.dev@gmail.com>
2021-10-20 21:15:05 +0200

    Enhance MyArtScene ui

--- client/src/components/MyArtScene/ImageGallery/ImageGallery.styles.ts
+++ client/src/components/MyArtScene/ImageGallery/ImageGallery.styles.ts
@@ -5,21 +5,36 @@ export const useStyles = makeStyles((theme) => ({
     flex: 1,
     display: 'flex',
     alignItems: 'center',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    width: 'min-content'
   },
+  rootContentContainer: {},
   imageContainer: {
     flex: 1,
-    height: theme.spacing(40),
-    minWidth: theme.spacing(40),
+    height: theme.spacing(60),
+    minWidth: theme.spacing(60),
     maxWidth: theme.spacing(60),
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: theme.spacing(4),
     '& > span': {
       width: '100%',
-      height: '100%'
+      flexShrink: 0,
+      flex: 1,
+      display: 'flex!important',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      overflow: 'hidden',
+      borderRadius: theme.spacing(5)
     }
   },
+  tokenName: {
+    flex: 0,
+    marginBottom: theme.spacing(1)
+  },
+  tokenLink: {},
   image: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
     objectFit: 'cover'
   }
 }));
