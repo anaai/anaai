38fdd4d6912043c0481d3a0d423470c2e74939d3
Filip Kis <filip.kis.dev@gmail.com>
2021-10-24 21:38:57 +0200

    Extract theme gradient

--- client/src/components/RootView/RootView.styles.ts
+++ client/src/components/RootView/RootView.styles.ts
@@ -1,10 +1,11 @@
 import { makeStyles } from '@material-ui/core';
+import { themeGradient } from 'config/theme/primaryTheme';
 
 export const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     minHeight: '100vh',
-    background: `linear-gradient(200deg, #00555d88 0%, #18abc887 35%, #17ac7f88 100%), linear-gradient(160deg, #00555d88 0%, #18abc887 35%, #17ac7f88 100%)`,
+    background: themeGradient,
     display: 'flex',
     flexDirection: 'column'
   },
