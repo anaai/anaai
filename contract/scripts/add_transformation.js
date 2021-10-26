6c26ffa5c3f77c52ece839f6d403e9b073b95c39
vonum <vonum.mk@gmail.com>
2021-10-26 13:09:20 +0200

    Update script for adding transformations

--- contract/scripts/add_transformation.js
+++ contract/scripts/add_transformation.js
@@ -15,18 +15,26 @@ web3.eth.defaultAccount = account.address;
 const contract = require("../artifacts/contracts/StyleArt.sol/StyleArt.json");
 const nftContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS);
 
-async function addTransformation(contract, address, name, price, supply) {
-  const message = await contract.methods.addTransformation(name, price, supply)
+async function addTransformation(contract, address, name, desc, price, supply) {
+  const message = await contract.methods.addTransformation(name, desc, price, supply)
                                         .send({from: address, gas: 500000});
   console.log(message)
 }
 
+asciiDesc = "Text art, also called ASCII art or keyboard art is a copy-pasteable digital age art form. It's about making text pictures with text symbols.";
+sketchDesc = "A sketch is a rapidly executed freehand drawing that is not usually intended as a finished work.";
+candyDesc = "Style transfer is the task of changing the style of an image in one domain to the style of an image in another domain.";
+feathersDesc = "Style transfer is the task of changing the style of an image in one domain to the style of an image in another domain.";
+mosaicDesc = "Style transfer is the task of changing the style of an image in one domain to the style of an image in another domain.";
+theScreamDesc = "Style transfer is the task of changing the style of an image in one domain to the style of an image in another domain.";
+udnieDesc = "Style transfer is the task of changing the style of an image in one domain to the style of an image in another domain.";
+
 (async () => {
-  await addTransformation(nftContract, PUBLIC_KEY, "ascii", 0, 300);
-  await addTransformation(nftContract, PUBLIC_KEY, "sketch", 0, 300);
-  await addTransformation(nftContract, PUBLIC_KEY, "candy", 0, 300);
-  await addTransformation(nftContract, PUBLIC_KEY, "feathers", 0, 300);
-  await addTransformation(nftContract, PUBLIC_KEY, "mosaic", 0, 300);
-  await addTransformation(nftContract, PUBLIC_KEY, "the_scream", 0, 300);
-  await addTransformation(nftContract, PUBLIC_KEY, "udnie", 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "ascii", asciiDesc, 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "sketch", sketchDesc, 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "candy", candyDesc, 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "feathers", feathersDesc, 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "mosaic", mosaicDesc, 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "theScream", theScreamDesc, 0, 300);
+  await addTransformation(nftContract, PUBLIC_KEY, "udnie", udnieDesc, 0, 300);
 })();
