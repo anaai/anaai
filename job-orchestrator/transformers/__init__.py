8607dc241b72da8c862b42f09c46498221ed18fc
vonum <vonum.mk@gmail.com>
2021-11-09 12:32:45 +0100

    Add task for anime gans

--- job-orchestrator/transformers/__init__.py
+++ job-orchestrator/transformers/__init__.py
@@ -1,3 +1,4 @@
 from transformers.ascii_art import ASCIIArt
 from transformers.sketch_art import SketchArt
 from transformers.fast_neural_style import FastNeuralStyle
+from transformers.anime_gan import AnimeGAN
