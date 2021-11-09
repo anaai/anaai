e8884bc66c59956bf74d92850d1791590a3867d0
vonum <vonum.mk@gmail.com>
2021-11-09 12:17:38 +0100

    Add anime gan models

--- job-orchestrator/transformers/model_paths.py
+++ job-orchestrator/transformers/model_paths.py
@@ -3,3 +3,7 @@ FEATHERS_FAST_NEURAL_TRANSFER_MODEL = "models/feathers.t7"
 MOSAIC_FAST_NEURAL_TRANSFER_MODEL = "models/mosaic.t7"
 THE_SCREAM_FAST_NEURAL_TRANSFER_MODEL = "models/the_scream.t7"
 UDNIE_FAST_NEURAL_TRANSFER_MODEL = "models/udnie.t7"
+
+CELEBA_DISTILL_ANIME_GAN = "models/celeba_distill.pt"
+FACE_PAINT_ANIME_GAN = "models/face_paint_512_v2.pt"
+PAPRIKA_ANIME_GAN = "models/paprika.pt"
