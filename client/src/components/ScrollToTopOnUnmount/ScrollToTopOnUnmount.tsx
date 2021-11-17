bdd7341d6564e0e39a649dedc34a042f49f5acd1
Filip Kis <filip.kis.dev@gmail.com>
2021-11-17 21:57:41 +0100

    Add ScrollToTopOnUnmount component type

--- client/src/components/ScrollToTopOnUnmount/ScrollToTopOnUnmount.tsx
+++ client/src/components/ScrollToTopOnUnmount/ScrollToTopOnUnmount.tsx
@@ -2,10 +2,8 @@ import { useEffect } from 'react';
 
 export const mainScrollContainerId = 'main-scroll-container';
 
-export const ScrollToTopOnUnmount = () => {
-  useEffect(() => {
-    return () => document.getElementById(mainScrollContainerId)?.scrollTo(0, 0);
-  }, []);
+export const ScrollToTopOnUnmount: React.FC<Record<string, unknown>> = () => {
+  useEffect(() => () => document.getElementById(mainScrollContainerId)?.scrollTo(0, 0), []);
 
   return null;
 };
