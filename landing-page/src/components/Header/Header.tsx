c787a5490b006343ddd9613c10d3eb97e1305856
vonum <vonum.mk@gmail.com>
2021-11-18 09:20:41 +0100

    Point to prod app url in landing page

--- landing-page/src/components/Header/Header.tsx
+++ landing-page/src/components/Header/Header.tsx
@@ -71,7 +71,7 @@ export const Header: React.FC<Record<string, unknown>> = () => {
                 size="small"
                 className={classes.goToAppButton}
                 target="_blank"
-                href="https://stg.anaai.art/"
+                href="https://app.anaai.art/"
                 color="secondary"
                 variant="contained"
                 rel="noopener noreferrer"
@@ -131,7 +131,7 @@ export const Header: React.FC<Record<string, unknown>> = () => {
                 size="small"
                 className={classes.goToAppButton}
                 target="_blank"
-                href="https://stg.anaai.art/"
+                href="https://app.anaai.art/"
                 color="secondary"
                 variant="contained"
                 rel="noopener noreferrer"
