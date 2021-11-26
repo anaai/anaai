b78a92bb213209faa2e54cc0beee451bde345b3c
vonum <vonum.mk@gmail.com>
2021-11-26 20:09:20 +0100

    Add more logs to nft service

--- nft-service/tests/test_logger.py
+++ nft-service/tests/test_logger.py
@@ -1,5 +1,5 @@
 import logger
 
 def test_log_token_minted(logs):
-  logger.log_token_minted(13)
-  assert "Token minted with id: 13." in logs.info
+  logger.log_token_minted("0x123", 13, "http://token_uri.com")
+  assert "Token minted with id: 13 for address: 0x123 at uri: http://token_uri.com." in logs.info
